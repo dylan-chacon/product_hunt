0d09f93bbac209a2610223719c1a71dac274dbcb
Dylan <dachaconp@unal.edu.co>
2021-02-02 12:32:23 -0500

    form sing in

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import { css } from '@emotion/react';
 import Layout from '../components/layout/Layout';
+import { Form, Field, InputSubmit } from '../components/UI/Form';
 
 const SignIn = () => {
     return (
         <div>
             <Layout>
-                <h1>Inicio</h1>
+                <>
+                    <h1
+                        css={css`
+                            text-align: center;
+                            margin-top: 5rem;
+                        `}
+                    >Crear Cuenta</h1>
+                    <Form>
+                        <Field>
+                            <label htmlFor="name">Nombre</label>
+                            <input
+                                type="text"
+                                id="name"
+                                placeholder="Tu nombre"
+                                name="name"
+                            />
+                        </Field>
+                        <Field>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="Tu email"
+                                name="email"
+                            />
+                        </Field>
+                        <Field>
+                            <label htmlFor="password">Contraseña</label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Contraseña"
+                                name="password"
+                            />
+                        </Field>
+                        <InputSubmit type="submit" value="Crear Cuenta" />
+                    </Form>
+                </>
             </Layout>
         </div>
     );
