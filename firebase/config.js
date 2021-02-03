31a66a1ccb5aa10b30f1b88f9fc0b6237e70f8c9
Dylan <dachaconp@unal.edu.co>
2021-02-03 16:09:13 -0500

    initialize firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1Aet5XKJET10D8zyTG4rE4EL1kDpvVb0",
+    authDomain: "product-hunt-c877b.firebaseapp.com",
+    projectId: "product-hunt-c877b",
+    storageBucket: "product-hunt-c877b.appspot.com",
+    messagingSenderId: "932303486835",
+    appId: "1:932303486835:web:f5a361f2c330c1ef8c0f06",
+    measurementId: "G-XT8NPPXC5X"
+};
+
+export default firebaseConfig;
