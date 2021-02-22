2658e9536083071b913ffbb69173d128d8e18063
dylan1496 <dachaconp@unal.edu.co>
2021-02-22 13:27:46 -0500

    handle comments

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -16,10 +16,21 @@ const ContenedorProducto = styled.div`
    }
 `;
 
+const Creator = styled.p`
+    padding: .5rem 2rem;
+    background-color: #DA552F;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    display: inline-block;
+    text-align: center;
+`;
+
 const Details = () => {
     //states
     const [product, setProduct] = useState({});
     const [error, setError] = useState(false);
+    const [comment, setComment] = useState({});
     const { comments, createdAt, description, name, company, url, urlImage, votes, creator, voted } = product;
 
     //router
@@ -54,6 +65,24 @@ const Details = () => {
         // atualizar state
         setProduct({ ...product, votes: newVotes, voted: [...voted, user.uid], });
     };
+    const handleComment = e => setComment({ ...comment, [e.target.name]: e.target.value });
+    const addComment = (e) => {
+        e.preventDefault();
+
+        if (!user) return router.push('login');
+        // extra info
+        comment.userID = user.uid;
+        comment.userName = user.displayName;
+        // db
+        firebase.db.collection('products').doc(id).update({
+            comments: [...product.comments, comment]
+        })
+        // state
+        setProduct({ ...product, comments: [...product.comments, comment]});
+    };
+    const isCreator = (id) => {
+        if (creator.id === id) return true;
+    };
 
     return (
         <>
@@ -75,26 +104,43 @@ const Details = () => {
                             {user && (
                                 <>
                                     <h2>Agrega tu comentario</h2>
-                                    <form>
+                                    <form onSubmit={() => addComment()}>
                                         <Field>
                                             <input
                                                 type="text"
                                                 name="message"
+                                                onChange={handleComment}
                                             />
                                         </Field>
-                                        <InputSubmit type="submit" value="Add comment" />
+                                        <InputSubmit type="submit" value="Agrega un comentario" />
                                     </form>
                                 </>
                             )}
                             <h2 css={css`
                                 margin: 2rem 0;
                             `}>Comentarios:</h2>
-                            {comments && comments.map((comment) => {
-                                <li>
-                                    <p>{comment.name}</p>
-                                    <p>Escrito por: {comment.userName}</p>
-                                </li>
-                            })}
+                            {comments.length === 0 ? (<p>Aún no hay comentarios</p>) : (
+                                <ul>
+                                    {comments && comments.map((comment, i) => (
+                                        <li
+                                            key={i}
+                                            css={css`
+                                            border: 1px solid #e1e1e1;
+                                            padding: 2rem;
+                                        `}>
+                                            <p>{comment.message}</p>
+                                            <p>Escrito por: 
+                                                <span css={css`
+                                                    font-weight: bold;
+                                                `}>{''}{comment.userName}</span>
+                                            </p>
+                                            {isCreator(comment.userID) && (
+                                                <Creator>Es creador</Creator>
+                                            )}
+                                        </li>
+                                    ))}
+                                </ul>  
+                            )}
                         </div>
                         <aside>
                             <Button
