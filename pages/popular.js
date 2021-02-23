8207d1c45e2041371ad8e2843c428a0b02c5056c
dylan1496 <dachaconp@unal.edu.co>
2021-02-23 11:35:05 -0500

    popular

--- pages/popular.js
+++ pages/popular.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import Layout from '../components/layout/Layout';
+import { FirebaseContext } from '../firebase';
+import Product from '../components/layout/Product';
 
-const Popular = () => {
-    return (
-        <div>
-            <Layout>
-                <h1>Popular</h1>
-            </Layout>
+export default function Popular() {
+  // state
+  const [products, setProducts] = useState([]);
+
+  //context
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const getProducts = () => {
+      firebase.db.collection('products').orderBy('votes', 'desc').get()
+        .then((snapshot) => {
+          const products = [];
+          snapshot.forEach((doc) => {
+            const product = {
+              ...doc.data(),
+              id: doc.id,
+            };
+            products.push(product);
+          });
+          setProducts(products);
+        });
+    }
+    getProducts();
+  }, [])
+
+  return (
+    <div>
+      <Layout>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {products.map(product => (
+                <Product key={product.id} product={product} />
+              ))}
+            </ul>
+          </div>
         </div>
-    );
-}
- 
-export default Popular;
+      </Layout>
+    </div>
+  )
+};
